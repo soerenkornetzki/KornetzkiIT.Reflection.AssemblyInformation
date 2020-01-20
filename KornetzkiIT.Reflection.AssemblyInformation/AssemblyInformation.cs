ce9ae428946c4828e3959993c0e7ec84c559100c
Sören Kornetzki <sk@kornetzkiit.com>
2020-01-20 13:17:40 +0100

    Initial import

---
+++ KornetzkiIT.Reflection.AssemblyInformation/AssemblyInformation.cs
@@ -0,0 +1,242 @@
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+namespace KornetzkiIT.Reflection.AssemblyInformation
+{
+    /// <summary>
+    /// A .NET Standard class for quering common assembly attributes out of an specified assembly.
+    /// </summary>
+    public class AssemblyInformation
+    {
+        #region Class Properties
+
+        /// <summary>
+        /// The specified target assembly.
+        /// </summary>
+        /// <seealso cref="Assembly"/>
+        public Assembly TargetAssembly { get; }
+
+        /// <summary>
+        /// Gets a Guid that represents the target assembly <see cref="GuidAttribute"/>.
+        /// </summary>
+        public Guid? Guid { get; }
+
+        /// <summary>
+        /// Gets a string that represents the target assembly <see cref="AssemblyProductAttribute"/>.
+        /// </summary>
+        public string Product { get; }
+
+        /// <summary>
+        /// Gets a string that represents the target assembly <see cref="AssemblyTitleAttribute"/>.
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// Gets a string that represents the target assembly <see cref="AssemblyDescriptionAttribute"/>.
+        /// </summary>
+        public string Description { get; }
+
+        /// <summary>
+        /// Gets a string that represents the target assembly <see cref="AssemblyCompanyAttribute"/>.
+        /// </summary>
+        public string Company { get; }
+
+        /// <summary>
+        /// Gets a string that represents the target assembly <see cref="AssemblyCopyrightAttribute"/>.
+        /// </summary>
+        public string Copyright { get; }
+
+        /// <summary>
+        /// Gets a string that represents the target assembly <see cref="AssemblyTrademarkAttribute"/>.
+        /// </summary>
+        public string Trademark { get; }
+
+        /// <summary>
+        /// Gets a string that represents the target assembly <see cref="AssemblyConfigurationAttribute"/>.
+        /// </summary>
+        public string Configuration { get; }
+
+        /// <summary>
+        /// Gets a CultureInfo that represents the target assembly <see cref="AssemblyCultureAttribute"/>.
+        /// </summary>
+        public CultureInfo Culture { get; }
+
+        /// <summary>
+        /// Gets a CultureInfo that represents the target assembly <see cref="NeutralResourcesLanguageAttribute"/>.
+        /// </summary>
+        public CultureInfo NeutralResourcesLanguage { get; }
+
+        /// <summary>
+        /// Gets a boolean that represents the target assembly <see cref="ComVisibleAttribute"/>.
+        /// </summary>
+        public bool? ComVisible { get; }
+
+        /// <summary>
+        /// Gets a boolean that represents the target assembly <see cref="CLSCompliantAttribute"/>.
+        /// </summary>
+        public bool? CLSCompliant { get; }
+
+        /// <summary>
+        /// Gets a <see cref="System.Version"/> that represents the target assembly <see cref="AssemblyVersionAttribute"/>.
+        /// </summary>
+        public Version Version { get; }
+
+        /// <summary>
+        /// Gets a <see cref="System.Version"/> that represents the target assembly <see cref="AssemblyFileVersionAttribute"/>.
+        /// </summary>
+        public Version FileVersion { get; }
+
+        /// <summary>
+        /// Gets a string that represents the target assembly <see cref="AssemblyInformationalVersionAttribute"/>.
+        /// </summary>
+        /// <remarks>Returns <c>null</c> when the attribute could not be determined.</remarks>
+        public string InformationalVersion { get; }
+
+        #endregion
+
+        #region Class Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblyInformation"/>
+        /// class with the target assembly being the calling assembly.
+        /// </summary>
+        /// <seealso cref="AssemblyInformation(Assembly)"/>
+        public AssemblyInformation() : this(Assembly.GetCallingAssembly()) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AssemblyInformation"/>
+        /// class with a specified target assembly.
+        /// </summary>
+        /// <param name="assembly">The specified target assembly.</param>
+        public AssemblyInformation(Assembly assembly)
+        {
+            TargetAssembly = assembly ?? throw new ArgumentNullException("assembly");
+
+            try { Guid = System.Guid.Parse(((GuidAttribute)TargetAssembly.GetCustomAttribute(typeof(GuidAttribute))).Value); }
+            catch (Exception e)
+            {
+                Guid = null;
+                Debug.Fail(string.Format("Could not extract GuidAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract GuidAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { Product = ((AssemblyProductAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyProductAttribute))).Product; }
+            catch (Exception e)
+            {
+                Product = null;
+                Debug.Fail(string.Format("Could not extract AssemblyProductAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyProductAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { Title = ((AssemblyTitleAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyTitleAttribute))).Title; }
+            catch (Exception e)
+            {
+                Title = null;
+                Debug.Fail(string.Format("Could not extract AssemblyTitleAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyTitleAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { Description = ((AssemblyDescriptionAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyDescriptionAttribute))).Description; }
+            catch (Exception e)
+            {
+                Description = null;
+                Debug.Fail(string.Format("Could not extract AssemblyDescriptionAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyDescriptionAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { Company = ((AssemblyCompanyAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyCompanyAttribute))).Company; }
+            catch (Exception e)
+            {
+                Company = null;
+                Debug.Fail(string.Format("Could not extract AssemblyCompanyAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyCompanyAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { Copyright = ((AssemblyCopyrightAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyCopyrightAttribute))).Copyright; }
+            catch (Exception e)
+            {
+                Copyright = null;
+                Debug.Fail(string.Format("Could not extract AssemblyCopyrightAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyCopyrightAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { Trademark = ((AssemblyTrademarkAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyTrademarkAttribute))).Trademark; }
+            catch (Exception e)
+            {
+                Trademark = null;
+                Debug.Fail(string.Format("Could not extract AssemblyTrademarkAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyTrademarkAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { Configuration = ((AssemblyConfigurationAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyConfigurationAttribute))).Configuration; }
+            catch (Exception e)
+            {
+                Configuration = null;
+                Debug.Fail(string.Format("Could not extract AssemblyConfigurationAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyConfigurationAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { Culture = new CultureInfo(((AssemblyCultureAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyCultureAttribute))).Culture); }
+            catch (Exception e)
+            {
+                Culture = null;
+                Debug.Fail(string.Format("Could not extract AssemblyCultureAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyCultureAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { NeutralResourcesLanguage = new CultureInfo(((NeutralResourcesLanguageAttribute)TargetAssembly.GetCustomAttribute(typeof(NeutralResourcesLanguageAttribute))).CultureName); }
+            catch (Exception e)
+            {
+                NeutralResourcesLanguage = null;
+                Debug.Fail(string.Format("Could not extract NeutralResourcesLanguageAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract NeutralResourcesLanguageAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { ComVisible = ((ComVisibleAttribute)TargetAssembly.GetCustomAttribute(typeof(ComVisibleAttribute))).Value; }
+            catch (Exception e)
+            {
+                ComVisible = null;
+                Debug.Fail(string.Format("Could not extract ComVisibleAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract ComVisibleAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { CLSCompliant = ((CLSCompliantAttribute)TargetAssembly.GetCustomAttribute(typeof(CLSCompliantAttribute))).IsCompliant; }
+            catch (Exception e)
+            {
+                CLSCompliant = null;
+                Debug.Fail(string.Format("Could not extract CLSCompliantAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract CLSCompliantAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { Version = System.Version.Parse(((AssemblyVersionAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyVersionAttribute))).Version); }
+            catch (Exception e)
+            {
+                Version = null;
+                Debug.Fail(string.Format("Could not extract AssemblyVersionAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyVersionAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { FileVersion = System.Version.Parse(((AssemblyFileVersionAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyFileVersionAttribute))).Version); }
+            catch (Exception e)
+            {
+                FileVersion = null;
+                Debug.Fail(string.Format("Could not extract AssemblyFileVersionAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyFileVersionAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+
+            try { InformationalVersion = ((AssemblyInformationalVersionAttribute)TargetAssembly.GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute))).InformationalVersion; }
+            catch (Exception e)
+            {
+                InformationalVersion = null;
+                Debug.Fail(string.Format("Could not extract AssemblyInformationalVersionAttribute: {0} {1}", e.GetType().ToString(), e.Message), e.StackTrace);
+                Trace.TraceWarning("Could not extract AssemblyInformationalVersionAttribute: {0} {1}", e.GetType().ToString(), e.Message);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ KornetzkiIT.Reflection.AssemblyInformation/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("bdd40b20-b15c-4b84-825d-39cd7c479ea8")]
+
+[assembly: AssemblyProduct("KornetzkiIT.Reflection.AssemblyInformation")]
+[assembly: AssemblyTitle("KornetzkiIT.Reflection.AssemblyInformation")]
+[assembly: AssemblyDescription("A .NET Standard class for quering common assembly attributes out of an specified assembly.")]
+[assembly: AssemblyCompany("Kornetzki IT")]
+[assembly: AssemblyCopyright("Copyright © 2019-2020 Sören Kornetzki")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
+
+#if DEBUG && TRACE
+[assembly: AssemblyConfiguration("Debug+Trace")]
+#elif DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#elif TRACE
+[assembly: AssemblyConfiguration("Release+Trace")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(true)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("0.1.0.1")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha.1")]
